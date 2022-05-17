05f19a5ba9b69788bbf93cf316307f40450472ef
Teguh Ainul Darajat <teguh.darajat@koinworks.com>
2022-05-17 10:36:13 +0700

    fix bug

--- main.go
+++ main.go
@@ -10,13 +10,18 @@ import (
 
 func main() {
 	students := make(map[string]models.Student)
-	req1 := params.CreateStudent("Ujang", "ujang@email.com", "Bandung", "Student", "Pro Backend Engineer")
+	req1 := params.CreateStudent("Teguh Ainul Darajat", "teguh@email.com", "Indonesia", "Junior Backend Engineer", "Pro Backend Engineer")
 	req2 := params.CreateStudent("Ujang", "ujang@email.com", "Bandung", "Student", "Pro Backend Engineer")
 
 	students["1"] = repositories.InsertStudent(req1)
 	students["2"] = repositories.InsertStudent(req2)
 
 	arguments := os.Args[1:]
+
+	if len(arguments) == 0 {
+		fmt.Println("Please input student ID")
+		return
+	}
 
 	for _, argument := range arguments {
 		if _, ok := students[argument]; !ok {
